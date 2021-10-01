8592967e6ee06ecf64c29bbd90b35994f5d565ed
zhyu <angellwings@gmail.com>
2021-10-02 07:40:29 +0900

    move nvim confs to nvim directory

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ function link_dotfile() {
   readonly filepath=${1:?"The filepath must be specified."}
   readonly dot_filename=${2:?"The dot_filename must be specified."}
 
-  if [[ -f $filepath ]]; then
+  if [[ -f $filepath || -d $filepath ]]; then
     if [[ -L $filepath ]]; then
       echo "$filepath is already a symlink, skipping linking it to $PWD/$dot_filename"
       return
@@ -26,8 +26,4 @@ function link_dotfile() {
 link_dotfile $HOME/.zshrc zshrc
 link_dotfile $HOME/.zprofile zprofile
 link_dotfile $HOME/.tmux.conf tmux.conf
-# TODO: link nvim directory?
-mkdir -p $HOME/.config/nvim/lua
-link_dotfile $HOME/.config/nvim/init.vim init.vim
-link_dotfile $HOME/.config/nvim/lua/plugins.lua plugins.lua
-link_dotfile $HOME/.config/nvim/coc-settings.json coc-settings.json
+link_dotfile $HOME/.config/nvim nvim
