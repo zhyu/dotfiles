4cb6f25bd45470bee6b452773864885ae6bd8f3b
zhyu <angellwings@gmail.com>
2021-10-22 20:57:58 +0900

    show devicons and bufferline

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -67,11 +67,19 @@ require('packer').startup(function(use)
     }
     use {
         'nvim-lualine/lualine.nvim',
+        requires = {'kyazdani42/nvim-web-devicons', opt = true },
         config = function()
             require('lualine').setup {
                 options = {
-                    icons_enabled = false,
                     theme = 'nightfox'
+                },
+                tabline = {
+                    lualine_a = { "buffers" },
+                    lualine_b = {},
+                    lualine_c = {},
+                    lualine_x = {},
+                    lualine_y = {},
+                    lualine_z = {}
                 }
             }
         end
