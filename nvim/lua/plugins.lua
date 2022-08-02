1fee1acdc3b67792573ebf7ce12e0a829d5243e8
zhyu <angellwings@gmail.com>
2022-08-02 04:26:11 +0000

    update luasnip config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -142,7 +142,7 @@ require('packer').startup(function(use)
                     'L3MON4D3/LuaSnip',
                     event = 'InsertEnter',
                     config = function()
-                        require('luasnip.loaders.from_vscode').lazy_load()
+                        require('plugins.luasnip')
                     end,
                     requires = {
                         {
---
+++ nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,8 @@
+-- Loading any vscode style snippets from plugins
+require('luasnip.loaders.from_vscode').lazy_load()
+
+-- Mappings to move around inside snippets
+vim.keymap.set('i', '<C-j>', function () require("luasnip").jump(1) end)
+vim.keymap.set('i', '<C-k>', function () require("luasnip").jump(-1) end)
+vim.keymap.set('s', '<C-j>', function () require("luasnip").jump(1) end)
+vim.keymap.set('s', '<C-k>', function () require("luasnip").jump(-1) end)
