d9df0bc6e6708c07965feb68a08821cf4c8b7228
zhyu <angellwings@gmail.com>
2022-08-12 16:53:47 +0900

    update nvim-cmp keymapping

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -67,7 +67,7 @@ cmp.setup({
 		["<C-u>"] = cmp.mapping.scroll_docs(-4),
 		["<C-d>"] = cmp.mapping.scroll_docs(4),
 		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
 
 		["<C-j>"] = cmp.mapping(function(fallback)
 			local luasnip = require("luasnip")
