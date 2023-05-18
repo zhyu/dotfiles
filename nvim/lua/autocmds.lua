50e85edf2c459059378e78489105f896fc230cae
zhyu <angellwings@gmail.com>
2023-05-18 09:01:07 +0000

    map lspsaga actions with commands instead of internal lua functions

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -33,17 +33,13 @@ create_aucmd("LspAttach", {
 			vim.keymap.set(mode, lhs, rhs, opts)
 		end
 
-		bufmap("n", "K", function()
-			require("lspsaga.hover").render_hover_doc()
-		end, "Display hover information about the symbol under the cursor")
+		bufmap("n", "K", "<cmd>Lspsaga hover_doc<CR>", "Display hover information about the symbol under the cursor")
 
 		bufmap("n", "gd", function()
 			require("telescope.builtin").lsp_definitions()
 		end, "Jump to the definition")
 
-		bufmap("n", "gf", function()
-			require("lspsaga.finder"):lsp_finder()
-		end, "Find definitions, implementations, and references")
+		bufmap("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", "Find definitions, implementations, and references")
 
 		bufmap("n", "gi", function()
 			require("telescope.builtin").lsp_implementations()
@@ -57,28 +53,34 @@ create_aucmd("LspAttach", {
 			require("telescope.builtin").lsp_references()
 		end, "List all the references")
 
-		bufmap("n", "gs", function()
-			require("lspsaga.signaturehelp").signature_help()
-		end, "Display a function's signature information")
+		bufmap("n", "<Leader>rn", "<cmd>Lspsaga rename<CR>", "Rename all references to the symbol under the cursor")
 
-		bufmap("n", "<Leader>rn", function()
-			require("lspsaga.rename"):lsp_rename()
-		end, "Rename all references to the symbol under the cursor")
+		bufmap(
+			"n",
+			"<Leader>ac",
+			"<cmd>Lspsaga code_action<CR>",
+			"Select a code action available at the current cursor position"
+		)
+		bufmap(
+			"x",
+			"<Leader>ac",
+			"<cmd>Lspsaga code_action<CR>",
+			"Select a code action available at the current cursor position"
+		)
 
-		bufmap("n", "<Leader>ac", function()
-			require("lspsaga.codeaction"):code_action()
-		end, "Select a code action available at the current cursor position")
-		bufmap("x", "<Leader>ac", function()
-			require("lspsaga.codeaction"):range_code_action()
-		end, "Select a code action available at the current cursor position")
-
-		bufmap("n", "gl", function()
-			require("lspsaga.diagnostic"):show_diagnostics("", "line")
-		end, "Show diagnostics of the current line in a floating window")
+		bufmap(
+			"n",
+			"gl",
+			"<cmd>Lspsaga show_line_diagnostics<CR>",
+			"Show diagnostics of the current line in a floating window"
+		)
 		-- gc is used to toggle comments, so use gp (point/position) instead
-		bufmap("n", "gp", function()
-			require("lspsaga.diagnostic"):show_diagnostics("", "cursor")
-		end, "Show diagnostics of the current cursor position in a floating window")
+		bufmap(
+			"n",
+			"gp",
+			"<cmd>Lspsaga show_cursor_diagnostics<CR>",
+			"Show diagnostics of the current cursor position in a floating window"
+		)
 
 		bufmap("n", "<Leader>fd", function()
 			require("telescope.builtin").diagnostics({ bufnr = 0 })
@@ -88,13 +90,9 @@ create_aucmd("LspAttach", {
 			require("telescope.builtin").diagnostics()
 		end, "List all diagnostics for all open buffers")
 
-		bufmap("n", "<Leader>pd", function()
-			require("lspsaga.diagnostic"):goto_prev()
-		end, "Move to the previous diagnostic")
+		bufmap("n", "<Leader>pd", "<cmd>Lspsaga diagnostic_jump_prev<CR>", "Move to the previous diagnostic")
 
-		bufmap("n", "<Leader>nd", function()
-			require("lspsaga.diagnostic"):goto_next()
-		end, "Move to the next diagnostic")
+		bufmap("n", "<Leader>nd", "<cmd>Lspsaga diagnostic_jump_next<CR>", "Move to the next diagnostic")
 	end,
 })
 
