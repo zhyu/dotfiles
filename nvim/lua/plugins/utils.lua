85527966fb5b3f17937d0289ac6b78f34cc6a331
zhyu <angellwings@gmail.com>
2023-07-21 13:52:34 +0900

    set key mappings for leap to repeat search

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -3,7 +3,17 @@ return {
 		"ggandor/leap.nvim",
 		event = "BufRead",
 		config = function()
-			require("leap").add_default_mappings()
+			local leap = require("leap")
+
+			leap.add_default_mappings()
+			leap.add_repeat_mappings(";", ",", {
+				-- False by default. If set to true, the keys will work like the
+				-- native semicolon/comma, i.e., forward/backward is understood in
+				-- relation to the last motion.
+				relative_directions = true,
+				-- By default, all modes are included.
+				modes = { "n", "x", "o" },
+			})
 		end,
 	},
 	{
