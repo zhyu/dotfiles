f76d0aea3d53022f639c59c45dcc8190a6591f3a
zhyu <angellwings@gmail.com>
2023-07-21 13:51:59 +0900

    replace abolish with text-case

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -17,10 +17,7 @@ return {
 		event = { "BufReadPost", "BufNewFile" },
 		config = true,
 	},
-	{
-		"tpope/vim-abolish",
-		event = "BufRead",
-	},
+	{ "johmsalas/text-case.nvim", event = { "BufReadPost", "BufNewFile" }, config = true },
 	-- library used by other plugins
 	{ "nvim-lua/plenary.nvim", lazy = true },
 }
