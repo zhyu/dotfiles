c0184c7a346d49dcd636fe0a060047b0a4db7c6e
zhyu <angellwings@gmail.com>
2023-12-15 20:36:48 +0900

    update setup_macos.sh to be compatible with both server and client usages

--- setup_macos.sh
+++ setup_macos.sh
@@ -2,5 +2,6 @@
 
 brew update
 brew upgrade
-brew install fx et bat fd ripgrep xh eza neovim tmux 1password-cli
+brew install fx bat fd ripgrep xh eza neovim tmux
+[[ $MACOS_SERVER != "1" ]] && echo "brew install MisterTea/et/et 1password-cli"
 brew cleanup
