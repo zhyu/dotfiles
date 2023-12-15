2f97e54a1ade492f3c681dcacbf579c4081bbca0
zhyu <angellwings@gmail.com>
2023-12-15 12:05:24 +0900

    update fzf-conf

--- zsh/ohmyzsh/custom/plugins/fzf-conf/fzf-conf.plugin.zsh
+++ zsh/ohmyzsh/custom/plugins/fzf-conf/fzf-conf.plugin.zsh
@@ -1,15 +1,19 @@
-# use fd as backend of fzf, it will respect ignore files like .gitignore
-export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+# Note: FZF_DEFAULT_COMMAND should have been set to use fd by fzf plugin of omz
+
 # use Ctrl-T to fuzzy search recent accessed files and directories
 export FZF_CTRL_T_COMMAND='fasd -Rl'
 # Use fd instead of the default find command for listing path candidates.
 # - The first argument to the function ($1) is the base path to start traversal
 # - See the source code (completion.{bash,zsh}) for the details.
+#
+# This function is used when using fzf to complete the path, e.g., `vim **<tab>`
 _fzf_compgen_path() {
   fd --hidden --follow --exclude ".git" . "$1"
 }
 
 # Use fd to generate the list for directory completion
+#
+# This function is used when using fzf to complete the dir, e.g., `cd **<tab>`
 _fzf_compgen_dir() {
   fd --type d --hidden --follow --exclude ".git" . "$1"
 }
