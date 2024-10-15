ec13dc472cd2acb6770517f54497c7b77da95d4f
zhyu <angellwings@gmail.com>
2024-10-15 06:39:12 +0000

    make it possible to run callbacks after pulling remote repos

--- pull_remotes.sh
+++ pull_remotes.sh
@@ -9,9 +9,17 @@ echo "| Pulling remote repos... |"
 echo "==========================="
 
 function pull_remote() {
-  readonly remote_url=${1:?"The remote_url must be specified."}
-  readonly subtree_dir=${2:?"The subtree_dir must be specified."}
-  readonly remote_ref=${3:-"master"}
+  readonly json_conf=${1:?"The json_conf must be specified."}
+
+  readonly remote_url=$(echo $json_conf | fx '.repo || ""')
+  readonly subtree_dir=$(echo $json_conf | fx '.gitSubtreeDir || ""')
+  readonly remote_ref=$(echo $json_conf | fx '.ref || "master"')
+  readonly callback=$(echo $json_conf | fx '.callback || ""')
+
+  : ${remote_url:?"The repo must be specified in the json_conf."}
+  : ${subtree_dir:?"The gitSubtreeDir must be specified in the json_conf."}
+
+  echo "Pulling remote repo: $remote_url into $subtree_dir"
 
   # parse remote name from remote url: last part of url without .git
   local remote_name=$(echo $remote_url | awk -F/ '{print $NF}' | sed 's/\.git$//')
@@ -20,8 +28,14 @@ function pull_remote() {
   git remote add $remote_name $remote_url
 
   git subtree pull --prefix=$subtree_dir $remote_name $remote_ref --squash
+
+  if [ -n "$callback" ]; then
+      echo "Running callback: $callback"
+      eval $callback
+  fi
+  echo "------------------------------------------------------------"
 }
 
-yq -oj remote_repos.yml | fx  '.map(x => [x.repo, x.gitSubtreeDir, x.ref].join(" "))' '.join("\n")' | while read line; do
-  pull_remote $(echo $line | awk '{print $1, $2, $3}')
+yq -oj remote_repos.yml | fx  '.map(x => JSON.stringify(x))' '.join("\n")' | while read line; do
+  pull_remote $line
 done
