1e06e52fcedfd14342298afad6b0ef9e4749a9ef
zhyu <angellwings@gmail.com>
2025-04-01 21:52:13 +0900

    migrate to vim.diagnostic.config.signs from deprecated sign_define

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -26,21 +26,22 @@ return {
 			---
 			-- Diagnostic Config
 			---
-			local sign = function(opts)
-				vim.fn.sign_define(opts.name, {
-					texthl = opts.name,
-					text = opts.text,
-					numhl = "",
-				})
-			end
-
-			sign({ name = "DiagnosticSignError", text = "✘" })
-			sign({ name = "DiagnosticSignWarn", text = "▲" })
-			sign({ name = "DiagnosticSignHint", text = "⚑" })
-			sign({ name = "DiagnosticSignInfo", text = "" })
-
 			vim.diagnostic.config({
 				severity_sort = true,
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = "✘",
+						[vim.diagnostic.severity.WARN] = "▲",
+						[vim.diagnostic.severity.INFO] = "",
+						[vim.diagnostic.severity.HINT] = "⚑",
+					},
+					numhl = {
+						[vim.diagnostic.severity.ERROR] = "",
+						[vim.diagnostic.severity.WARN] = "",
+						[vim.diagnostic.severity.INFO] = "",
+						[vim.diagnostic.severity.HINT] = "",
+					},
+				},
 			})
 		end,
 		dependencies = {
